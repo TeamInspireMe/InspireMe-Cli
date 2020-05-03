9975d18455e2ee4532f0911448e58eab90f68b03
phimth <tphiman@gmail.com>
2020-05-03 16:56:00 +0200

    fix UI

--- inspire_me/lib/library/globals.dart
+++ inspire_me/lib/library/globals.dart
@@ -3,6 +3,8 @@ library my_prj.globals;
 import 'package:inspire_me/class/postclass.dart';
 import 'package:inspire_me/class/user.dart';
 
+bool isLogged = false;
+
 int cpt = 10;
 
 List<Post> listPosts = [
