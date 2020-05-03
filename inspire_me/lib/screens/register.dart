b045e9ea571454cd6f9146b6ad14a5f0efcaee0d
phimth <tphiman@gmail.com>
2020-05-03 23:08:25 +0200

    front for register

--- inspire_me/lib/screens/register.dart
+++ inspire_me/lib/screens/register.dart
@@ -11,14 +11,17 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  final _formKey = GlobalKey<FormState>();
+  final _signUpKey = GlobalKey<FormState>();
   String username;
   String email;
   String password;
   String confirmPassword;
 
   register() {
-    print('register');
+    print(_signUpKey.currentState.validate());
+    if (_signUpKey.currentState.validate()) {
+      print('${username} ${email} ${password} ${confirmPassword}');
+    }
     //globals.isLogged = true;
   }
 
@@ -62,106 +65,38 @@ class _RegisterState extends State<Register> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        reverse: true,
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-              maxHeight: MediaQuery.of(context).size.height - 56 -
-                  MediaQuery.of(context).padding.top),
-          child: Container(
-            color: HexColor('#F25C54'),
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Image.asset('assets/inspiration.png'),
-                        Text(
-                          'UP',
-                          style: TextStyle(fontSize: 40),
-                        ),
-                      ]),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Form(
-                      child: Column(
-                    children: <Widget>[
-                      Text('Username', style: TextStyle(fontSize: 20)),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              bottomRight: Radius.circular(10)),
-                        ),
-                        margin: EdgeInsets.symmetric(
-                            horizontal:
-                                MediaQuery.of(context).size.width * 0.15,
-                            vertical: 10),
-                        child: TextFormField(validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your username';
-                          }
-                          return null;
-                        },
-                        onChanged: (text) {
-                          setState(() {
-                            username = text;
-                          });
-                        },),
-                      ),
-                      Text('Email', style: TextStyle(fontSize: 20)),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              bottomRight: Radius.circular(10)),
-                        ),
-                        margin: EdgeInsets.symmetric(
-                            horizontal:
-                                MediaQuery.of(context).size.width * 0.15,
-                            vertical: 10),
-                        child: TextFormField(validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your email';
-                          }else if(RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)){
-                            return 'Please enter a correct email';
-                          }
-                          return null;
-                        },
-                        onChanged: (text) {
-                          setState(() {
-                            email = text;
-                          });
-                        },),
-                      ),
-                      Text('Password', style: TextStyle(fontSize: 20)),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              bottomRight: Radius.circular(10)),
-                        ),
-                        margin: EdgeInsets.symmetric(
-                            horizontal:
-                                MediaQuery.of(context).size.width * 0.15,
-                            vertical: 10),
-                        child: TextFormField(validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your title';
-                          }
-                          return null;
-                        },
-                        onChanged: (text) {
-                          setState(() {
-                            title = text;
-                          });
-                        },),
-                      ),
-                      Text('Confirm Password', style: TextStyle(fontSize: 20)),
-                      Card(
+      body: Form(
+        key: _signUpKey,
+        child: SingleChildScrollView(
+          reverse: true,
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+                maxHeight: MediaQuery.of(context).size.height -
+                    56 -
+                    MediaQuery.of(context).padding.top),
+            child: Container(
+              color: HexColor('#F25C54'),
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image.asset('assets/inspiration.png'),
+                          Text(
+                            'UP',
+                            style: TextStyle(fontSize: 40),
+                          ),
+                        ]),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Form(
+                        child: Column(
+                      children: <Widget>[
+                        Text('Username', style: TextStyle(fontSize: 20)),
+                        Card(
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.only(
                                 topLeft: Radius.circular(10),
@@ -171,41 +106,126 @@ class _RegisterState extends State<Register> {
                               horizontal:
                                   MediaQuery.of(context).size.width * 0.15,
                               vertical: 10),
-                          child: TextFormField(validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your title';
-                          }
-                          return null;
-                        },
-                        onChanged: (text) {
-                          setState(() {
-                            title = text;
-                          });
-                        },)),
-                      SizedBox(height: 20),
-                      Container(
-                        width: 175,
-                        child: RawMaterialButton(
-                          fillColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              side: BorderSide(color: Colors.red)),
-                          onPressed: () {
-                            register();
-                          },
-                          child: Text('Register'),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter your username';
+                              }
+                              return null;
+                            },
+                            onChanged: (text) {
+                              setState(() {
+                                username = text;
+                              });
+                            },
+                          ),
                         ),
-                      ),
-                      FlatButton(
-                          onPressed: () {
-                            goToLogin();
-                          },
-                          child: Text("Already have an account ?",
-                              style: TextStyle(fontSize: 15)))
-                    ],
-                  )),
-                )
-              ],
+                        Text('Email', style: TextStyle(fontSize: 20)),
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10)),
+                          ),
+                          margin: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.15,
+                              vertical: 10),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter your email';
+                              } else if (!RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(value)) {
+                                return 'Please enter a correct email';
+                              }
+                              return null;
+                            },
+                            onChanged: (text) {
+                              setState(() {
+                                email = text;
+                              });
+                            },
+                          ),
+                        ),
+                        Text('Password', style: TextStyle(fontSize: 20)),
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10)),
+                          ),
+                          margin: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.15,
+                              vertical: 10),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter your password';
+                              } else if (value.length < 8) {
+                                return 'Please enter a password with at least 8 characters';
+                              }
+                              return null;
+                            },
+                            onChanged: (text) {
+                              setState(() {
+                                password = text;
+                              });
+                            },
+                          ),
+                        ),
+                        Text('Confirm Password',
+                            style: TextStyle(fontSize: 20)),
+                        Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  bottomRight: Radius.circular(10)),
+                            ),
+                            margin: EdgeInsets.symmetric(
+                                horizontal:
+                                    MediaQuery.of(context).size.width * 0.15,
+                                vertical: 10),
+                            child: TextFormField(
+                              validator: (value) {
+                                if (value != password) {
+                                  return 'Passwords are not the same';
+                                }
+                                return null;
+                              },
+                              onChanged: (text) {
+                                setState(() {
+                                  confirmPassword = text;
+                                });
+                              },
+                            )),
+                        SizedBox(height: 20),
+                        Container(
+                          width: 175,
+                          child: RawMaterialButton(
+                            fillColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: BorderSide(color: Colors.red)),
+                            onPressed: () {
+                              register();
+                            },
+                            child: Text('Register'),
+                          ),
+                        ),
+                        FlatButton(
+                            onPressed: () {
+                              goToLogin();
+                            },
+                            child: Text("Already have an account ?",
+                                style: TextStyle(fontSize: 15)))
+                      ],
+                    )),
+                  )
+                ],
+              ),
             ),
           ),
         ),
